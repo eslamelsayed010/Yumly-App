18ef1fb68f7010bcc4365175f1b13a9d7510746e
Eslam Elsayed <eslimelseyd@gmail.com>
2025-02-09 22:52:29 +0200

    add splash view and changes in ui

--- Yumly/app/src/main/java/com/example/yumly/ui/MainActivity.java
+++ Yumly/app/src/main/java/com/example/yumly/ui/MainActivity.java
@@ -1,37 +1,47 @@
 package com.example.yumly.ui;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.yumly.databinding.ActivityMainBinding;
+import com.example.yumly.databinding.SplashViewBinding;
 import com.example.yumly.presenters.AuthPresenter;
 import com.example.yumly.ui.auth.SignupUi;
 import com.example.yumly.views.AuthView;
 
 public class MainActivity extends AppCompatActivity implements AuthView {
 
-    AuthPresenter presenter;
-    ActivityMainBinding binding;
+    //AuthPresenter presenter;
+    SplashViewBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = SplashViewBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        presenter = new AuthPresenter(this);
-
-        binding.btnId.setOnClickListener(v -> {
-            Intent intent = new Intent(this, SignupUi.class);
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, SignupUi.class);
             startActivity(intent);
-            presenter.login();
-        });
+        },5000);
+
+        //presenter = new AuthPresenter(this);
+
+//        binding.btnId.setOnClickListener(v -> {
+//            Intent intent = new Intent(this, SignupUi.class);
+//            startActivity(intent);
+//            presenter.login();
+//        });
+
     }
 
     @Override
     public void onGetData(String name, String password) {
-        binding.txtId.setText(name);
+        //binding.txtId.setText(name);
     }
 }
