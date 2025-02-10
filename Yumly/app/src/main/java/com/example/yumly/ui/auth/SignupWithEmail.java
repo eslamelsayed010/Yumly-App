a4880d80f1e5d56bfc5afd706c769a2b2a067202
Eslam Elsayed <eslimelseyd@gmail.com>
2025-02-10 23:42:49 +0200

    sign up with with firebase

--- Yumly/app/src/main/java/com/example/yumly/ui/auth/SignupWithEmail.java
+++ Yumly/app/src/main/java/com/example/yumly/ui/auth/SignupWithEmail.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.yumly.databinding.ActivitySignupWithEmailBinding;
 import com.google.firebase.auth.FirebaseAuth;
