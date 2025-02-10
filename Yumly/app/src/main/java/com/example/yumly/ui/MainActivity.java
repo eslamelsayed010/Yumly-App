b550abb14e96309a558aa712022075ac8b168d2c
Eslam Elsayed <eslimelseyd@gmail.com>
2025-02-10 20:22:57 +0200

    sign in with google

--- Yumly/app/src/main/java/com/example/yumly/ui/MainActivity.java
+++ Yumly/app/src/main/java/com/example/yumly/ui/MainActivity.java
@@ -7,11 +7,9 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.yumly.databinding.SplashViewBinding;
 import com.example.yumly.ui.auth.SignupUi;
-import com.example.yumly.views.AuthView;
 
-public class MainActivity extends AppCompatActivity implements AuthView {
+public class MainActivity extends AppCompatActivity {
 
-    //AuthPresenter presenter;
     SplashViewBinding binding;
 
     @Override
@@ -26,18 +24,6 @@ public class MainActivity extends AppCompatActivity implements AuthView {
             startActivity(intent);
         },5000);
 
-        //presenter = new AuthPresenter(this);
-
-//        binding.btnId.setOnClickListener(v -> {
-//            Intent intent = new Intent(this, SignupUi.class);
-//            startActivity(intent);
-//            presenter.login();
-//        });
-
     }
 
-    @Override
-    public void onGetData(String name, String password) {
-        //binding.txtId.setText(name);
-    }
 }
